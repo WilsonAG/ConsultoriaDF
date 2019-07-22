7e5780b290698bad45c7cbef4164e64387fc44f3
echangom <echangom@est.ups.edu.ec>
2019-07-21 23:18:21 -0500

    Agregada clase CausaFetal

---
+++ consulta/src/Model/CausaFetal.java
@@ -0,0 +1,34 @@
+package Model;
+
+public class CausaFetal {
+	private int id_causa_fetal;
+	private String nombre;
+
+	public CausaFetal(int id_causa_fetal, String nombre) {
+		super();
+		this.id_causa_fetal = id_causa_fetal;
+		this.nombre = nombre;
+	}
+
+	public int getId_causa_fetal() {
+		return id_causa_fetal;
+	}
+
+	public void setId_causa_fetal(int id_causa_fetal) {
+		this.id_causa_fetal = id_causa_fetal;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "CausaFetal [id_causa_fetal=" + id_causa_fetal + ", nombre=" + nombre + "]";
+	}
+
+}
