5756dd33c0b71f2880461bc27694730d70f7678a
Wilson Aguilar <waguilars@est.ups.edu.ec>
2019-07-23 00:14:34 -0500

    Agregado Grafico pastel para consulta general
    creado metodos para crear grafico

--- consulta/src/Control/controlConsultas.java
+++ consulta/src/Control/controlConsultas.java
@@ -4,10 +4,15 @@ import java.io.Serializable;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.pie.PieChartDataSet;
+import org.primefaces.model.charts.pie.PieChartModel;
 
 import Conexion.Conexion;
 import Model.Provincia;
@@ -18,11 +23,43 @@ public class controlConsultas implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Provincia> provincias;
+	private PieChartModel pastel;
 
 	@PostConstruct
 	public void init() {
 		cargarMuertesXProvinca();
+		graficarPastel();
 	}
+	
+	public void graficarPastel() {
+		pastel = new PieChartModel();
+		ChartData data = new ChartData();
+		
+		PieChartDataSet dataset = new PieChartDataSet();
+		ArrayList<Number> valores = new ArrayList<Number>();
+		ArrayList<String> colores = new ArrayList<String>();
+		for (Provincia p : provincias) {
+			valores.add(p.getMuertes());
+			colores.add(generarColor());
+		}
+		dataset.setData(valores);
+		dataset.setBackgroundColor(colores);
+		
+		data.addChartDataSet(dataset);
+		ArrayList<String> labels = new ArrayList<String>();
+		for (Provincia p : provincias) {
+			labels.add(p.getNombre());
+		}
+		
+		data.setLabels(labels);
+		
+		
+		pastel.setData(data);
+		
+		
+	}
+	
+	
 
 	private void cargarMuertesXProvinca() {
 		provincias = new ArrayList<Provincia>();
@@ -48,6 +85,14 @@ public class controlConsultas implements Serializable {
 
 	}
 	
+	private String generarColor() {
+		Random rnd = new Random();
+		int n1 = rnd.nextInt(255);
+		int n2 = rnd.nextInt(255);
+		int n3 = rnd.nextInt(255);
+		return "rgb("+n1+", "+n2+", "+n3+")";
+	}
+	
 	public ArrayList<Provincia> getProvincias() {
 		return provincias;
 	}
@@ -56,11 +101,17 @@ public class controlConsultas implements Serializable {
 		this.provincias = provincias;
 	}
 	
+	public PieChartModel getPastel() {
+		return pastel;
+	}
+	
+	public void setPastel(PieChartModel pastel) {
+		this.pastel = pastel;
+	}
 	
 	@Override
 	public String toString() {
 		return "controlConsultas [provincias=" + provincias + "]";
 	}
-
 
 }
